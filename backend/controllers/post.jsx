c47b3ec07674f21a97a5b6b23c8d9be4dd0e8191
Abhik8261 <abhik.tech8261@gmail.com>
2023-02-27 22:02:10 +0530

    delete comment

--- backend/controllers/post.jsx
+++ backend/controllers/post.jsx
@@ -163,7 +163,7 @@ exports.addComment = async (req, res) => {
     }
 
     let commentindex = -1;
-    
+
     post.comments.forEach((item, index) => {
       if (item.user.toString() === req.user._id.toString()) {
        commentindex = index;
@@ -195,3 +195,57 @@ exports.addComment = async (req, res) => {
     });
   }
 };
+
+
+exports.deleteComment=async (req,res)=>{
+    try {
+        const post=await Post.findById(req.params.id);
+        if(!post){
+          return res.status(404).json({
+            success:false,
+            message:"Post not found"
+          })
+
+        }
+
+
+        if(post.owner.toString===req.user._id.toString){
+
+            if(req.body.commentId==undefined){
+                return res.status(400).json({
+                    success:false,
+                    message:"Comment Id is required"
+                })
+            }
+
+            post.comments.forEach((item, index) => {
+                if (item._id.toString() === req.body.commentId.toString()) {
+                    return post.comments.splice(index,1)
+                }
+              });
+              await post.save();
+              res.status(200).json({
+                success:true,
+                message:"slected comment is deleted"
+              })
+        }
+        else{
+            post.comments.forEach((item, index) => {
+                if (item.user.toString() === req.user._id.toString()) {
+              return post.comments.splice(index,1)
+                }
+              });
+              await post.save();
+              res.status(200).json({
+                success:true,
+                message:"your comment is deleted"
+              })
+        }
+        
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        })
+    }
+}
