83aee199f86de91102e0701dbd5a102c29ad8527
Abhik8261 <abhik.tech8261@gmail.com>
2023-03-05 10:14:47 +0530

    header

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const User=require('../models/User');
 const Post=require('../models/Post')
 const { sendEmail } = require("../Middleware/sendEmail")
-
+const crypto = require("crypto");
 exports.register=async(req,res)=>{
     try {
         const{name,email,password}=req.body;
@@ -338,6 +338,45 @@ exports.forgetPassword=async(req,res)=>{
        
 
         res.status(500).json({ 
+            success:false,
+            message:error.message,
+        })
+    }
+}
+
+exports.resetPassword=async(req,res)=>{
+    try {
+        const resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(req.params.token)
+        .digest("hex");
+  
+      const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() },
+      });
+  
+      if (!user) {
+        return res.status(401).json({
+          success: false,
+          message: "Token is invalid or has expired",
+        });
+      }
+  
+      user.password = req.body.password;
+  
+      user.resetPasswordToken = undefined;
+      user.resetPasswordExpire = undefined;
+      await user.save();
+  
+      res.status(200).json({
+        success: true,
+        
+        message: "Password Updated",
+    });
+
+    } catch (error) {
+        res.status(500).json({
             success:false,
             message:error.message,
         })
---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './Header/Header';
+
+function App() {
+  return (
+    
+      <>
+      <Router>
+     <Header/>
+      </Router>
+      
+      </>
+   
+  );
+}
+
+export default App;
